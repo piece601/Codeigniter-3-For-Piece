158a4b241dca2243fd6d2d0c86f4c550c63e844d
Piece Chao <piece601@hotmail.com>
2015-04-01 21:17:20 +0800

    Initial

---
+++ application/views/_templates/_header.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="zh-Hant-TW">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta name="description" content="描述">
+  <meta name="keywords"content="關鍵字,關鍵字">
+  <meta name="author" content="趙承瑋 Piece">
+  <title>Framework for Piece.</title>
+  <link rel="stylesheet" type="text/css" href="<?=base_url('assets/css/bootstrap.min.css')?>">
+  <link rel="stylesheet" type="text/css" href="<?=base_url('assets/css/bootstrap-theme.min.css')?>">
+  <link rel="stylesheet" type="text/css" href="<?=base_url('assets/css/animate.css')?>">
+  <!--[if lt IE 9]>
+    <script src="//cdnjs.cloudflare.com/ajax/libs/html5shiv/3.7.2/html5shiv.min.js"></script>
+    <script src="//cdnjs.cloudflare.com/ajax/libs/respond.js/1.4.2/respond.min.js"></script>
+  <![endif]-->
+  <script type="text/javascript" src="<?=base_url('assets/js/jquery.min.js')?>"></script>
+  <script type="text/javascript" src="<?=base_url('assets/js/bootstrap.min.js')?>"></script>
+</head>
+<body>
---
+++ application/views/test/file_single.php
@@ -0,0 +1,24 @@
+<? @require_once '_templates/_header.php';?>
+
+<article class="container">
+	<section class="row">
+
+		<form class="form-horizontal" role="form" method="post" action="" enctype="multipart/form-data">
+		  <div class="form-group">
+		    <label class="col-sm-2 control-label">檔案位置</label>
+		    <div class="col-sm-9">
+	        <input class="form-control" type="file" name="userfile" />
+		    </div>
+		  </div>
+
+		  <div class="form-group">
+		    <div class="col-sm-offset-2 col-sm-10">
+		      <button type="submit" class="btn btn-primary btn-lg">送出</button>
+		    </div>
+		  </div>
+		</form>
+
+	</section>
+</article>
+
+<? @require_once '_templates/_footer.php';?>
